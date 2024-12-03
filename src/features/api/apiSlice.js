c279dc58a30b3c4b4760de65bd50c1a3aeb8ffe7
vivek <vivek.kumar@cnetric.com>
2024-12-03 19:24:00 +0530

    Update apiSlice

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,13 +3,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://98.70.53.81:3005/",
+    baseUrl: "https://apiservice.ukyc.universalcommerce.io",
     prepareHeaders: (headers, { getState }) => {
-   
-      const token = getState().auth.token; 
+      const token = getState().auth.token;
       // console.log(token, "token------------")
       if (token) {
-        headers.set("Authorization", `Bearer ${token}`); 
+        headers.set("Authorization", `Bearer ${token}`);
       }
       return headers;
     },
